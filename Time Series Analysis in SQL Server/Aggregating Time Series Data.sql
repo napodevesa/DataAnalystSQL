42f8f1bd246938292667bf42d61b4c24366c8e23
napodevesa <napodevesa@gmail.com>
2022-03-05 12:53:48 -0300

    first commit

---
+++ Time Series Analysis in SQL Server/Aggregating Time Series Data.sql
@@ -0,0 +1,41 @@
+	-- Fill in the functions and columns
+SELECT
+	it.IncidentType,
+	COUNT(1) AS NumberOfRows,
+	SUM(ir.NumberOfIncidents) AS TotalNumberOfIncidents,
+	MIN(ir.NumberOfIncidents) AS MinNumberOfIncidents,
+	MAX(ir.NumberOfIncidents) AS MaxNumberOfIncidents,
+	MIN(ir.IncidentDate) As MinIncidentDate,
+	MAX(ir.IncidentDate) AS MaxIncidentDate
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.IncidentType it
+		ON ir.IncidentTypeID = it.IncidentTypeID
+WHERE
+	ir.IncidentDate BETWEEN '2019-08-01' AND '2019-10-31'
+GROUP BY
+	it.IncidentType;
+
+	-- Fill in the functions and columns
+SELECT
+	COUNT(DISTINCT ir.IncidentTypeID) AS NumberOfIncidentTypes,
+	COUNT(DISTINCT ir.IncidentDate) AS NumberOfDaysWithIncidents
+FROM dbo.IncidentRollup ir
+WHERE
+ir.IncidentDate BETWEEN '2019-08-01' AND '2019-10-31';
+
+	-- Fill in the functions and columns
+SELECT
+	it.IncidentType,
+    -- Fill in the appropriate expression
+	SUM(CASE WHEN ir.NumberOfIncidents > 5 THEN 1 ELSE 0 END) AS NumberOfBigIncidentDays,
+    -- Number of incidents will always be at least 1, so
+    -- no need to check the minimum value, just that it's
+    -- less than or equal to 5
+    SUM(CASE WHEN ir.NumberOfIncidents <= 5 THEN 1 ELSE 0 END) AS NumberOfSmallIncidentDays
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.IncidentType it
+		ON ir.IncidentTypeID = it.IncidentTypeID
+WHERE
+	ir.IncidentDate BETWEEN '2019-08-01' AND '2019-10-31'
+GROUP BY
+it.IncidentType;
