3580f698dd7f83bbc73aabc71cac44e075f8b848
napodevesa <napodevesa@gmail.com>
2022-03-08 10:07:37 -0300

    3.datacamp.sql

---
+++ 3.datacamp.sql
@@ -0,0 +1,135 @@
+DECLARE @string1 NVARCHAR(100) = 'Chocolate with beans from';
+DECLARE @string2 NVARCHAR(100) = 'has a cocoa percentage of';
+
+SELECT 
+	bean_type,
+	bean_origin,
+	cocoa_percent,
+	-- Create a message by concatenating values with "+"
+	@string1 + ' ' + bean_origin + ' ' + @string2 + ' ' + CAST(cocoa_percent AS nvarchar) AS message1
+FROM ratings
+WHERE 
+	company = 'Ambrosia' 
+	AND bean_type <> 'Unknown';
+
+---****---
+
+	DECLARE @string1 NVARCHAR(100) = 'Chocolate with beans from';
+DECLARE @string2 NVARCHAR(100) = 'has a cocoa percentage of';
+
+SELECT 
+	bean_type,
+	bean_origin,
+	cocoa_percent,
+	-- Create a message by concatenating values with "+"
+	@string1 + ' ' + bean_origin + ' ' + @string2 + ' ' + CAST(cocoa_percent AS nvarchar) AS message1,
+	-- Create a message by concatenating values with "CONCAT()"
+	CONCAT(@string1, ' ', bean_origin, ' ', @string2, ' ', cocoa_percent) AS message2
+FROM ratings
+WHERE 
+	company = 'Ambrosia' 
+	AND bean_type <> 'Unknown';
+
+---****---
+
+		DECLARE @string1 NVARCHAR(100) = 'Chocolate with beans from';
+DECLARE @string2 NVARCHAR(100) = 'has a cocoa percentage of';
+
+SELECT 
+	bean_type,
+	bean_origin,
+	cocoa_percent,
+	-- Create a message by concatenating values with "+"
+	@string1 + ' ' + bean_origin + ' ' + @string2 + ' ' + CAST(cocoa_percent AS nvarchar) AS message1,
+	-- Create a message by concatenating values with "CONCAT()"
+	CONCAT(@string1, ' ', bean_origin, ' ', @string2, ' ', cocoa_percent) AS message2,
+	-- Create a message by concatenating values with "CONCAT_WS()"
+	CONCAT_WS(' ', @string1, bean_origin, @string2, cocoa_percent) AS message3
+FROM ratings
+WHERE 
+	company = 'Ambrosia' 
+	AND bean_type <> 'Unknown';
+
+---****---
+
+			SELECT
+	-- Create a list with all bean origins, delimited by comma
+	STRING_AGG(bean_origin, ',') AS bean_origins
+FROM ratings
+WHERE company IN ('Bar Au Chocolat', 'Chocolate Con Amor', 'East Van Roasters');
+
+---****---
+
+SELECT 
+	company,
+    -- Create a list with all bean origins
+	STRING_AGG(bean_origin, ',') AS bean_origins
+FROM ratings
+WHERE company IN ('Bar Au Chocolat', 'Chocolate Con Amor', 'East Van Roasters')
+-- Specify the columns used for grouping your data
+GROUP BY company;
+
+---****---
+
+SELECT 
+	company,
+    -- Create a list with all bean origins ordered alplabetically
+	STRING_AGG(bean_origin, ',') WITHIN GROUP (ORDER BY bean_origin) AS bean_origins
+FROM ratings
+WHERE company IN ('Bar Au Chocolat', 'Chocolate Con Amor', 'East Van Roasters')
+-- Specify the columns used for grouping your data
+GROUP BY company;
+
+---****---
+
+DECLARE @phrase NVARCHAR(MAX) = 'In the morning I brush my teeth. In the afternoon I take a nap. In the evening I watch TV.'
+
+SELECT value
+FROM STRING_SPLIT(@phrase, '.');
+
+
+
+---****---
+
+SELECT
+	first_name,
+    last_name,
+	birthdate,
+	email,
+	country
+FROM voters
+   -- Select only voters with a first name less than 5 characters
+WHERE LEN(first_name) < 5
+   -- Look for the desired pattern in the email address
+	AND PATINDEX('j_a%@yahoo.com', email) > 0;
+---****---
+
+SELECT
+    -- Concatenate the first and last name
+	CONCAT('***' , first_name, ' ', UPPER(last_name), '***') AS name,
+    last_name,
+	birthdate,
+	email,
+	country
+FROM voters
+   -- Select only voters with a first name less than 5 characters
+WHERE LEN(first_name) < 5
+   -- Look for this pattern in the email address: "j%[0-9]@yahoo.com"
+	AND PATINDEX('j_a%@yahoo.com', email) > 0; 
+
+---****---  
+
+
+
+SELECT
+    -- Concatenate the first and last name
+	CONCAT('***' , first_name, ' ', UPPER(last_name), '***') AS name,
+    -- Mask the last two digits of the year
+    REPLACE(birthdate, SUBSTRING(CAST(birthdate AS varchar), 3, 2), 'XX') AS birthdate,
+	email,
+	country
+FROM voters
+   -- Select only voters with a first name less than 5 characters
+WHERE LEN(first_name) < 5
+   -- Look for this pattern in the email address: "j%[0-9]@yahoo.com"
+	AND PATINDEX('j_a%@yahoo.com', email) > 0;    
