fd3c7a75ee0c10fa22f1369d9c3e106be5bf060b
napodevesa <napodevesa@gmail.com>
2022-03-09 13:22:34 -0300

    continuing with the datacamp analitycs course

--- 1_DesignDB.sql
+++ 1_DesignDB.sql
@@ -64,3 +64,52 @@ ADD COLUMN author_id SERIAL PRIMARY KEY;
 
 -- Output the new table
 SELECT * FROM dim_author;
+
+
+----****----****----
+
+-- Output each state and their total sales_amount
+SELECT dim_store_star.state, sum(sales_amount)
+FROM fact_booksales
+	-- Join to get book information
+    JOIN dim_book_star on fact_booksales.book_id = dim_book_star.book_id
+	-- Join to get store information
+    JOIN dim_store_star on fact_booksales.store_id = dim_store_star.store_id
+-- Get all books with in the novel genre
+WHERE  
+    dim_book_star.genre = 'novel'
+-- Group results by state
+GROUP BY
+    dim_store_star.state;
+
+
+----****----****----
+
+    -- Output each state and their total sales_amount
+SELECT dim_state_sf.state, sum(sales_amount)
+FROM fact_booksales
+    -- Joins for the genre
+    JOIN dim_book_sf on fact_booksales.book_id = dim_book_sf.book_id
+    JOIN dim_genre_sf on dim_book_sf.genre_id = dim_genre_sf.genre_id
+    -- Joins for the state 
+    JOIN dim_store_sf on fact_booksales.store_id = dim_store_sf.store_id 
+    JOIN dim_city_sf on dim_store_sf.city_id = dim_city_sf.city_id
+	JOIN dim_state_sf on  dim_city_sf.state_id = dim_state_sf.state_id
+-- Get all books with in the novel genre and group the results by state
+WHERE  
+    dim_genre_sf.genre = 'novel'
+GROUP BY
+   dim_state_sf.state;
+
+----****----****----
+
+   -- Add a continent_id column with default value of 1
+ALTER TABLE dim_country_sf
+ADD continent_id int NOT NULL DEFAULT(1);
+
+-- Add the foreign key constraint
+ALTER TABLE dim_country_sf ADD CONSTRAINT country_continent
+   FOREIGN KEY (continent_id) REFERENCES dim_continent_sf(continent_id);
+   
+-- Output updated table
+SELECT * FROM dim_country_sf;
