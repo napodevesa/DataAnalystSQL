0e19184057b22d296f0f7c37faa435bf8266f935
napodevesa <napodevesa@gmail.com>
2022-03-10 12:20:43 -0300

    continuing course database design

--- 1_DesignDB.sql
+++ 1_DesignDB.sql
@@ -176,14 +176,36 @@ INNER JOIN labels
 ON labels.reviewid = high_scores.reviewid
 WHERE label = 'self-released';
 
+----****----****----
+-- Create a view with the top artists in 2017
+CREATE VIEW top_artists_2017 AS
+-- with only one column holding the artist field
+SELECT artist_title.artist FROM artist_title
+INNER JOIN top_15_2017
+ON artist_title.reviewid = top_15_2017.reviewid;
+
+-- Output the new view
+SELECT * FROM top_artists_2017;
+
+
+----****----****----
+-- Revoke everyone's update and insert privileges
+REVOKE UPDATE, INSERT ON long_reviews FROM PUBLIC; 
+
+-- Grant editor update and insert privileges 
+GRANT UPDATE, INSERT ON long_reviews TO editor; 
+
 
 ----****----****----
 
+-- Redefine the artist_title view to have a label column
+CREATE OR REPLACE VIEW artist_title AS
+SELECT reviews.reviewid, reviews.title, artists.artist, labels.label
+FROM reviews
+INNER JOIN artists
+ON artists.reviewid = reviews.reviewid
+INNER JOIN labels
+ON labels.reviewid = reviews.reviewid;
 
-
-----****----****----
-
-
-
-----****----****----
+SELECT * FROM artist_title;
 
