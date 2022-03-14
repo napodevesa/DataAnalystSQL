e0c9948ff555a4d58b86af6a7d4558ff8e53d65a
napodevesa <napodevesa@gmail.com>
2022-03-14 11:57:52 -0300

    continuing course database design 4

--- 1_DesignDB.sql
+++ 1_DesignDB.sql
@@ -209,6 +209,18 @@ ON labels.reviewid = reviews.reviewid;
 
 SELECT * FROM artist_title;
 ----****----****----
+-- Create a materialized view called genre_count 
+CREATE MATERIALIZED VIEW genre_count AS
+SELECT genre, COUNT(*) 
+FROM genres
+GROUP BY genre;
 
+INSERT INTO genres
+VALUES (50000, 'classical');
+
+-- Refresh genre_count
+REFRESH MATERIALIZED VIEW genre_count;
+
+SELECT * FROM genre_count;
 
 
