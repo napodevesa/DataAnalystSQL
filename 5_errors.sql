525324d8ef185bf3e2e298eadc44c0163baea333
napodevesa <napodevesa@gmail.com>
2022-03-15 23:42:30 -0300

    errors3

--- 5_errors.sql
+++ 5_errors.sql
@@ -45,3 +45,42 @@ BEGIN CATCH
     	SELECT 'An error occurred inserting the error!';
     END CATCH    
 END CATCH
+-----------------------------------
+-- Set up the TRY block
+BEGIN TRY	
+	SELECT 'Total: ' + SUM(price * quantity) AS total
+  	FROM orders 
+END TRY
+-- Set up the CATCH block
+BEGIN CATCH
+	-- Show error information.
+	SELECT  ERROR_NUMBER() AS number,  
+        	ERROR_SEVERITY() AS severity_level,  
+        	ERROR_STATE() AS state,
+        	ERROR_LINE() AS line,  
+        	ERROR_MESSAGE() AS message; 	    
+END CATCH 
+-----------------------------------
+
+BEGIN TRY
+    INSERT INTO products (product_name, stock, price) 
+    VALUES	('Trek Powerfly 5 - 2018', 2, 3499.99),   		
+    		('New Power K- 2018', 3, 1999.99)		
+END TRY
+-- Set up the outer CATCH block
+BEGIN CATCH
+	SELECT 'An error occurred inserting the product!';
+    -- Set up the inner TRY block
+    BEGIN TRY
+    	-- Insert the error
+    	INSERT INTO errors 
+        	VALUES ('Error inserting a product');
+    END TRY    
+    -- Set up the inner CATCH block
+    BEGIN CATCH
+    	-- Show number and message error
+    	SELECT 
+        	ERROR_LINE() AS line,	   
+			ERROR_MESSAGE() AS message; 
+    END CATCH    
+END CATCH
