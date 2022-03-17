6099bc5030647b1b7c0539171d47a10f601bb4af
napodevesa <napodevesa@gmail.com>
2022-03-17 20:07:39 -0300

    finish 7

--- 7_.sql
+++ 7_.sql
@@ -43,5 +43,52 @@ ELSE
    	SELECT * FROM staff WHERE staff_id = @staff_id
 
 ----***----
+DECLARE @first_name NVARCHAR(20) = 'Pedro';
+
+-- Concat the message
+DECLARE @my_message NVARCHAR(500) =
+  CONCAT('There is no staff member with ', @first_name, ' as the first name.');
+
+IF NOT EXISTS (SELECT * FROM staff WHERE first_name = @first_name)
+  -- Throw the error
+  THROW 50000, @my_message, 1;
+
+
+
+----***----
+
+
+  DECLARE @product_name AS NVARCHAR(50) = 'Trek CrossRip+ - 2018';
+-- Set the number of sold bikes
+DECLARE @sold_bikes AS INT = 10;
+DECLARE @current_stock INT;
+
+SELECT @current_stock = stock FROM products WHERE product_name = @product_name;
+
+DECLARE @my_message NVARCHAR(500) =
+  -- Customize the error message
+  FORMATMESSAGE('There are not enough %s bikes. You have %d in stock.', @product_name, @current_stock);
+
+IF (@current_stock - @sold_bikes < 0)
+  -- Throw the error
+  THROW 50000, @my_message, 1;
+
+
+----***----
+
+EXEC sp_addmessage @msgnum = 50002, @severity = 16, @msgtext = 'There are not enough %s bikes. You only have %d in stock.', @lang = N'us_english';
+
+DECLARE @product_name AS NVARCHAR(50) = 'Trek CrossRip+ - 2018';
+--Change the value
+DECLARE @sold_bikes AS INT = 10;
+DECLARE @current_stock INT;
+
+SELECT @current_stock = stock FROM products WHERE product_name = @product_name;
+
+DECLARE @my_message NVARCHAR(500) =
+  FORMATMESSAGE(50002, @product_name, @current_stock);
+
+IF (@current_stock - @sold_bikes < 0)
+  THROW 50000, @my_message, 1;
 
    	
