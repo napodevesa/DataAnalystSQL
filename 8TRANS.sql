918e1843758e3edf69c1b378da7b0b0380affec7
napodevesa <napodevesa@gmail.com>
2022-03-20 20:42:45 -0300

    finish trans

---
+++ 8TRANS.sql
@@ -0,0 +1,54 @@
+BEGIN TRY  
+	BEGIN TRAN; 
+		UPDATE accounts SET current_balance = current_balance - 100 WHERE account_id = 1;
+		INSERT INTO transactions VALUES (1, -100, GETDATE());
+        
+		UPDATE accounts SET current_balance = current_balance + 100 WHERE account_id = 5;
+		INSERT INTO transactions VALUES (5, 100, GETDATE());
+	COMMIT TRAN;     
+END TRY
+BEGIN CATCH  
+	ROLLBACK TRAN;
+END CATCH
+
+---**---
+BEGIN TRY  
+	-- Begin the transaction
+	BEGIN TRAN;
+		UPDATE accounts SET current_balance = current_balance - 100 WHERE account_id = 1;
+		INSERT INTO transactions VALUES (1, -100, GETDATE());
+        
+		UPDATE accounts SET current_balance = current_balance + 100 WHERE account_id = 5;
+        -- Correct it
+		INSERT INTO transactions VALUES (5, 100, GETDATE());
+    -- Commit the transaction
+	COMMIT TRAN;    
+END TRY
+BEGIN CATCH  
+	SELECT 'Rolling back the transaction';
+    -- Rollback the transaction
+	ROLLBACK TRAN;
+END CATCH
+---**---
+
+-- Begin the transaction
+BEGIN TRAN;
+	UPDATE accounts set current_balance = current_balance + 100
+		WHERE current_balance < 5000;
+	-- Check number of affected rows
+	IF @@ROWCOUNT > 200 
+		BEGIN 
+        	-- Rollback the transaction
+			ROLLBACK TRAN; 
+			SELECT 'More accounts than expected. Rolling back'; 
+		END
+	ELSE
+		BEGIN 
+        	-- Commit the transaction
+			COMMIT TRAN; 
+			SELECT 'Updates commited'; 
+		END
+		
+---**---
+---**---
+---**---
