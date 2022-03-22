bb8893534e7d7d08b3d29f0c84dcab590b9d5d7c
napodevesa <napodevesa@gmail.com>
2022-03-22 18:08:23 -0300

    avances

--- 10.sql
+++ 10.sql
@@ -24,4 +24,84 @@ GROUP BY
 		 WHEN DATEPART(SECOND, StartDate) > 0 THEN 'SECONDS > 0' END
 
 --***--
+SELECT
+    -- Select the day of week value for StartDate
+  DATENAME(weekday, StartDate) as DayOfWeek,
+    -- Calculate TotalTripHours
+  SUM(DATEDIFF(second, StartDate, EndDate))/ 3600 as TotalTripHours 
+FROM CapitalBikeShare 
+-- Group by the day of week
+GROUP BY DATENAME(weekday, StartDate)
+-- Order TotalTripHours in descending order
+ORDER BY TotalTripHours DESC
 
+--***--
+
+SELECT
+	-- Calculate TotalRideHours using SUM() and DATEDIFF()
+  	SUM(DATEDIFF(SECOND, StartDate, EndDate))/ 3600 AS TotalRideHours,
+    -- Select the DATE portion of StartDate
+  	CONVERT(DATE, StartDate) AS DateOnly,
+    -- Select the WEEKDAY
+  	DATENAME(WEEKDAY, CONVERT(DATE, StartDate)) AS DayOfWeek 
+FROM CapitalBikeShare
+-- Only include Saturday
+WHERE DATENAME(WEEKDAY, StartDate) = 'Saturday' 
+GROUP BY CONVERT(DATE, StartDate);
+
+--***--
+
+-- Create @ShiftStartTime
+DECLARE @ShiftStartTime AS time = '08:00 AM'
+
+-- Create @StartDate
+DECLARE @StartDate AS date
+
+-- Set StartDate to the first StartDate from CapitalBikeShare
+SET 
+	@StartDate = (
+    	SELECT TOP 1 StartDate 
+    	FROM CapitalBikeShare 
+    	ORDER BY StartDate ASC
+		)
+
+-- Create ShiftStartDateTime
+DECLARE @ShiftStartDateTime AS datetime
+
+-- Cast StartDate and ShiftStartTime to datetime data types
+SET @ShiftStartDateTime = CAST(@StartDate AS datetime) + CAST(@ShiftStartTime AS datetime) 
+
+SELECT @ShiftStartDateTime
+
+--***--
+-- Declare @Shifts as a TABLE
+DECLARE @Shifts TABLE(
+    -- Create StartDateTime column
+	StartDateTime datetime,
+    -- Create EndDateTime column
+	EndDateTime datetime)
+-- Populate @Shifts
+INSERT INTO @Shifts (StartDateTime, EndDateTime)
+	SELECT '3/1/2018 8:00 AM', '3/1/2018 4:00 PM' 
+SELECT * 
+FROM @Shifts
+--***--
+-- Declare @RideDates
+DECLARE @RideDates TABLE(
+    -- Create RideStart
+	RideStart date,
+    -- Create RideEnd
+	RideEnd date)
+-- Populate @RideDates
+INSERT INTO @RideDates(RideStart, RideEnd)
+-- Select the unique date values of StartDate and EndDate
+SELECT DISTINCT
+    -- Cast StartDate as date
+	CAST(StartDate as date),
+    -- Cast EndDate as date
+	CAST(EndDate as date) 
+FROM CapitalBikeShare 
+SELECT * 
+FROM @RideDates
+--***--
+--***--
